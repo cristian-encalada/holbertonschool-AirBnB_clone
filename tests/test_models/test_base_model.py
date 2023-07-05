e3bce64e1aa0efbf567e68ced7d3e8c33962fd9b
Aldo2303 <aldod03@gmail.com>
2023-07-05 16:20:21 -0700

    test BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
     """Unittest for the functions in BaseModel"""
     def test_type_attributes(self):
@@ -13,6 +14,24 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(type(base0.id), str)
         self.assertEqual(type(base0.created_at), datetime)
         self.assertEqual(type(base0.updated_at), datetime)
+
+    def test_created(self):
+        base1 = BaseModel()
+        base1.name = "My first name"
+        self.assertEqual(base1.name, "My first name")
+        base1.name = "My update name"
+        self.assertEqual(base1.name, "My update name")
+        base1.number = 87
+        self.assertEqual(base1.number, 87)
+
+    def test_inheritance(self):
+        base2 = BaseModel()
+        self.assertTrue(isinstance(base2, BaseModel))
+
+    def test_save(self):
+        base3 = BaseModel()
+        self.assertEqual(type(base3.updated_at), datetime)
+
 
 if __name__ == '__main__':
     unittest.main()
