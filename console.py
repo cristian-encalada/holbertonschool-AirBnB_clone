dcff5326e44ab04bd8fc38952203bc9c848c0157
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-06 12:44:04 -0300

    [6] Console 0.0.1

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Contains the entry point of the command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Contains the entry point of the command interpreter
+    """
+    prompt = "(hbnb)"
+
+    def do_EOF(self, line):
+        """EOF command to exit the program
+        """
+        exit()
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        quit()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
