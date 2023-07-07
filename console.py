016c76cb1ab3b18dc71431581cb62eda9f4504b6
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-07 20:20:02 -0300

    [8] Fix name command do_update

--- console.py
+++ console.py
@@ -130,7 +130,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
 
-    def update(self, line):
+    def do_update(self, line):
         """
         Updates an instance based on the class name and id
         by adding or updating attribute (save the change into the JSON file).
