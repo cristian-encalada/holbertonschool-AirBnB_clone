a43678032e1a462c95d09ab810a7611783e9810c
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-07-07 12:01:50 -0300

    [7] create command done

--- console.py
+++ console.py
@@ -2,12 +2,14 @@
 """ Contains the entry point of the command interpreter
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the entry point of the command interpreter
     """
-    prompt = "(hbnb)"
+    models = ["BaseModel"]
+    prompt = "(hbnb) "
 
     def emptyline(self):
         """Do nothing when an empty line is entered
@@ -23,6 +25,25 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program
         """
         quit()
+
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it(to the JSON file),
+        and prints the id
+        Usage: create <model>
+        """
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        args = args.split(' ')[0]
+        if args not in HBNBCommand.models:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = eval(args)()
+        new_instance.save()
+        print(new_instance.id)
 
 
 if __name__ == '__main__':
